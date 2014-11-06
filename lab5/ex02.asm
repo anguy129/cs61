d537894f2e07040c9a31cc18ad52ec328df48204
Rashid Goshtasbi <rgosh001@well.cs.ucr.edu>
2014-11-05 20:07:48 -0800

    final lab5 w/o ex03.asm

---
+++ lab5/ex02.asm
@@ -0,0 +1,216 @@
+;================================================
+; Name: <Goshtasbi, Rashid>
+; Username: rgosh001
+; 
+; Lab: <lab 5, Exercise 1>
+; Lab section: 023
+; TA: Bryan Marsh
+; 
+;=================================================
+
+.ORIG x3000
+   ;----------------------
+   ;Instructions
+   ;----------------------
+   LEA R0, PROMPT
+   PUTS
+   GETC
+   OUT
+
+   LD R1, SUB_COUNT_3400
+   JSRR R1
+
+   LEA R0, SKIPLINE
+   TRAP x24
+
+   LD R1, SUB_PRINT_OUT
+   JSRR R1
+
+   HALT
+    
+   ;---
+   ;LOCAL DATA
+   ;---
+   
+   PROMPT   .STRINGZ    "Enter Binary Number Starting With 'b': "
+   SKIPLINE .STRINGZ    "\n"
+   SUB_COUNT_3400 .FILL x3200
+   SUB_PRINT_OUT  .FILL x3400
+
+;---------------------------------------------------------------------------------   
+   ;---
+   ; SUBROUTINE #1
+   ;---
+
+   .ORIG x3200
+
+   ;SUBROUTINE INSTRUCTIONS
+   ;(1) Backup R7 & any registers the subroutine changes except Return Values
+      ST R7, BACKUP_R7_3200   ;backup R2
+
+   ; (2) Subroutine's algorithm
+
+      LD R1, COUNT_INPUT
+      GETC_LOOP
+         GETC
+         OUT
+
+         ADD R6, R1, #-1
+         BRz SKIP
+         
+         LD R4, CONVERT
+         ADD R3, R0, R4    ;convert input
+
+         ADD R5, R1, #-1
+         MULT_LOOP
+            ADD R3, R3, R3
+            ADD R5, R5, #-1
+            BRp MULT_LOOP
+         END_MULT_LOOP
+         BRz SKIP_ZERO
+
+         SKIP
+            ADD R3, R0, R4
+            BRp NUMB_ONE
+
+         NUMB_ONE
+            ADD R3, R3, #0
+         
+         SKIP_ZERO
+         ADD R2, R2, R3
+         LD R3, EMPTY
+         
+         ADD R1, R1, #-1
+         BRp GETC_LOOP
+      END_GETC_LOOP
+
+
+   ;(3) Restore Registers
+      LD R7, BACKUP_R7_3200   ;backup R2
+      
+
+   ;(4) Return
+      RET
+   ; Subroutine Data
+
+      BACKUP_R7_3200 .BLKW #1
+
+      COUNT_INPUT      .FILL       #16
+      CONVERT     .FILL       #-48
+      EMPTY       .FILL       #0
+
+
+      .END
+
+;---------------------------------------------------------------------------------   
+   ;---
+   ; SUBROUTINE #2
+   ;---
+
+   .ORIG x3400
+
+   ;SUBROUTINE INSTRUCTIONS
+   ;(1) Backup R7 & any registers the subroutine changes except Return Values
+      ST R7, BACKUP_R7_4000   ;backup R7
+      ST R2, BACKUP_R2_4000   ;backup R2
+
+   ; (2) Subroutine's algorithm
+      LEA R0, LETTER
+      TRAP x24
+      LD R3, COUNT            ;Holds count for loop for 16 times(aka 16-bits)   
+      LOOP
+         ADD R2, R2, #0       ;Make R2 LMR
+         BRp PRINT_ZERO       ;if R2 is pos, go to print_zero loop
+         BRn PRINT_ONE        ;if r2 is neg, go to print_one loop
+
+         PRINT_ZERO           ;loop to print zero is positive
+            LD R0, NUM_ONE    ;load #48, which is 0, to R0
+            TRAP x21          ;prints out R0 = 0
+            ADD R2, R2, R2    ;left shift by adding number to itself
+            ADD R3, R3, #-1   ;reduce COUNT aka the 16 bit count
+
+            LD R0, SPACE1     ;load R0 with space1 label
+            ADD R0, R0, R3    ;add count & space1
+            BRz PRINT_SPACE   ;if R0 is zero, go to PRINT_SPACE function to print space
+
+            LD R0, SPACE2     ;load R0 with space2 label
+            ADD R0, R0, R3    ;add count & space2
+            BRz PRINT_SPACE   ;if R0 is zero, go to PRINT_SPACE function to print space
+
+            LD R0, SPACE3     ;load R0 with space3 label
+            ADD R0, R0, R3    ;add count & space3
+            BRz PRINT_SPACE   ;if R0 is zero, go to PRINT_SPACE function to print space
+
+            ADD R3, R3, #0
+            BRp LOOP          ;goes back to begining of loop is NUM is positive
+            BRnz END_LOOP     ;goes to end of loop is NUM is zero or negative
+         END_PRINT_ZERO
+
+         PRINT_ONE            ;loop to print zero is positive
+
+            LD R0, NUM_ZERO   ;load #48, which is 0, to R0
+            TRAP x21          ;prints out R0 = 0
+            ADD R2, R2, R2    ;left shift by adding number to its
+            ADD R3, R3, #-1   ;reduce COUNT aka the 16 bit count
+
+            LD R0, SPACE1     ;load R0 with space1 label
+            ADD R0, R0, R3    ;add count & space1
+            BRz PRINT_SPACE   ;if R0 is zero, go to PRINT_SPACE function to print space
+
+            LD R0, SPACE2     ;load R0 with space2 label
+            ADD R0, R0, R3    ;add count & space2
+            BRz PRINT_SPACE   ;if R0 is zero, go to PRINT_SPACE function to print space
+
+            LD R0, SPACE3     ;load R0 with space3 label
+            ADD R0, R0, R3    ;add count & space3
+            BRz PRINT_SPACE   ;if R0 is zero, go to PRINT_SPACE function to print space
+
+            ADD R3, R3, #0
+            BRp LOOP         ;goes back to begining of loop is NUM is positive
+            BRnz END_LOOP     ;goes to end of loop is NUM is zero or negative
+         END_PRINT_ONE
+
+         PRINT_SPACE          ;function to print out a space ONLY BETWEEN every 4 bits
+            LD R0, SPACE_CHAR ;loads a space character into R0 for output
+            TRAP x21          ;outputs the space character
+            ADD R3, R3, #0    ;makes R3 LMR to check for loop iteration
+            BRp LOOP         ;if its positive, enter loop again
+            BRnz END_LOOP     ;if its negative or zero, exit loop, the HALT program
+         END_PRINT_SPACE
+
+      END_LOOP                ;END LOOP
+
+
+
+   ;(3) Restore Registers
+      LD R2, BACKUP_R2_4000   ;backup R2
+      LD R7, BACKUP_R7_4000   ;backup R7
+
+   ;(4) Return
+      RET
+
+   ; Subroutine Data
+
+      BACKUP_R2_4000 .BLKW #1
+      BACKUP_R7_4000 .BLKW #1
+
+      SPACE_CHAR  .STRINGZ    " "
+      NUM_ONE     .FILL       #48
+      NUM_ZERO    .FILL       #49
+      ADD_NUM     .FILL       #0
+      NUM         .FILL       xABCD
+      COUNT       .FILL       #16
+      COUNT3   .FILL    #10
+      COUNT2   .FILL    #9
+      LOCATION .FILL    x4000
+      NUM      .FILL    x30
+      SPACE1      .FILL       #-4
+      SPACE2      .FILL       #-8
+      SPACE3      .FILL       #-12
+
+      NUM_DEC     .STRINGZ    "\n"
+      LETTER      .STRINGZ    "b"
+
+      .END
+      
+;---------------------------------------------------------------------------------   
---
+++ lab5/ex04.asm
@@ -0,0 +1,178 @@
+;================================================
+; Name: <Goshtasbi, Rashid>
+; Username: rgosh001
+; 
+; Lab: <lab 5, Exercise 4>
+; Lab section: 023
+; TA: Bryan Marsh
+; 
+;=================================================
+
+.ORIG x3000
+   ;----------------------
+   ;Instructions
+   ;----------------------
+   
+   JSR SUB_COUNT_3400
+    
+   ;---
+   ;LOCAL DATA
+   ;---
+   
+   ; N/A
+
+   ;---
+   ; SUBROUTINE
+   ;---
+
+.ORIG x3400    ;PROGRAM BEGINS HERE
+   ;SUBROUTINE INSTRUCTIONS
+   ;(1) Backup R7 & any registers the subroutine changes except Return Values
+      SUB_COUNT_3400
+
+   ; (2) Subroutine's algorithm
+   START
+      LEA R0, PROMPT          ;outputs input prompt for the user
+      TRAP x22
+
+
+      LD R1, EMPTY            ;initialize R1 to 0
+      LD R3, EMPTY            ;for neg number
+
+      LD R5, EMPTY            ;to make sure no 2nd use or +/- signs
+      ADD R5, R5, #1
+
+      INPUT_LOOP
+         GETC                 ;get user input
+         OUT                  ;ouput it to the screen
+
+         ADD R7, R0, #-10     ;goes to multiplying and outputting the answer
+         BRz END_INPUT_LOOP
+
+         
+         LD R6, EMPTY         ;init R6 to 0 to use for check
+         LD R4, NEG           ;check for NEG# by negating negative dec values to compare to
+         NOT R4, R4           ;check to see it its a negative sign inputted
+         ADD R4, R4, #1
+         ADD R6, R0, R4
+         BRz NEG_LOOP         ;enters neg loop if it a neg sign
+         BRnp END_NEG_LOOP    ;skips neg loop if it isnt a neg sign
+
+         NEG_LOOP
+            LD R3, ONE        ;makes R7 = 1 as a flag to negate answer at the end
+            ADD R5, R5, #-1   ;checks to see if a second - was inputted = error
+            BRn ERROR1
+            BRzp INPUT_LOOP
+            ;BRp INPUT_LOOP    ;starts over again
+         END_NEG_LOOP
+
+
+         LD R6, EMPTY         ;init R6 to 0 to use for check
+         LD R4, POS           ;check for POS# by negating negative dec values to compare to
+         NOT R4, R4           
+         ADD R4, R4, #1
+         ADD R6, R0, R4
+         BRz POS_LOOP
+         BRnp END_POS_LOOP
+
+         POS_LOOP
+            ADD R5, R5, #-1       ;checks to see if a second + was inputted = error
+            BRn ERROR1
+            BRzp  INPUT_LOOP      ;if plus sign, re-enter loop as it has no effect
+         END_POS_LOOP
+
+         ;---reg number input---
+         REG_NUM
+            LD R6, EMPTY      ;makes R6 = 0
+            LD R4, CHAR_UP    ;load register with ascii value 9
+            NOT R4, R4
+            ADD R4, R4, #1
+            LD R6, EMPTY      
+            ADD R6, R0, R4    ;check if the character input is above char 9
+            BRp ERROR1        ;if it is, enter ERROR1 loop to restart program
+            BRnz END_ERROR    ;if isnt, skip over ERROR1 loop and do next check
+            ERROR1
+               LEA R0, ERROR
+               TRAP x22
+               ADD R3, R3, #1
+               BRnzp START
+            END_ERROR
+
+            LD R6, EMPTY      ;makes R6 = 0
+            LD R4, CHAR_DW    ;load register with ascii value 0
+            NOT R4, R4
+            ADD R4, R4, #1
+            LD R6, EMPTY
+            ADD R6, R0, R4    ;check if the character input is below char 0
+            BRn ERROR1        ;if it is, enter ERROR1 loop to restart program
+            BRzp END_ERROR    ;if isnt, skip over ERROR1 loop and do next check
+            ERROR1            ;error loop to print out message and restart program
+               LEA R0, ERROR
+               TRAP x22
+               ADD R3, R3, #1
+               BRnzp START
+            END_ERROR
+
+            LD R6, COUNT10    ;counter for multiplying number by 10
+            LD R4, EMPTY      ;init R4 to 0
+            ADD R4, R1, #0    ;copies value to multiply itself by 10
+            MULT_LOOP         ;multiply value by 10 loop
+               ADD R1, R1, R4
+               ADD R6, R6, #-1
+               BRp MULT_LOOP
+               BRz END_MULT_LOOP
+            END_MULT_LOOP
+            
+            LD R6, CONVERT
+            NOT R6, R6
+            ADD R6, R6, #1
+            ADD R0, R0, R6    ;convert to binary
+            ADD R1, R1, R0    ;added to R1
+
+            LD R6, EMPTY      ;repeats the loop indefinately unless by enter sign or error
+            ADD R6, R6, #0
+            BRnzp INPUT_LOOP
+         ENG_REG_NUM
+
+      END_INPUT_LOOP
+
+
+      ADD R3, R3, #0          ;LMS R3 to check if value inputed was negative
+      BRp NEG_ANSWER
+      BRnz END_NEG_ANSWER
+      NEG_ANSWER              ;loop to do 2's complement to negate answer
+         NOT R1, R1
+         ADD R1, R1, #1
+      END_NEG_ANSWER
+
+
+   HALT                    ;HALT program
+
+
+   ;(3) Restore Registers
+      
+
+   ;(4) Return
+   
+   ; Subroutine Data
+
+   ERROR       .STRINGZ    "\nError: Only input +/- sign with 00 to 20 only. Please Try Again\n"
+   PROMPT      .STRINGZ    "Input a number between 00 and 20\n"
+   PROMPTNEG   .STRINGZ    "NEGATIVE NUMBER INPUTTED\n"
+   PROMPTPOS   .STRINGZ    "POSITIVE NUMBER INPUTTED\n"
+   
+   ONE      .FILL       #1
+   EMPTY    .FILL       #0
+   CHAR_UP  .FILL       #57
+   CHAR_DW  .FILL       #48
+   ZERO     .FILL       #0
+   CONVERT  .FILL       #48
+   COUNT10  .FILL       #9
+   ENTER    .FILL       #10
+   POS      .FILL       #43
+   NEG      .FILL       #45
+
+   .ORIG x4000
+   .FILL #0
+
+.END
