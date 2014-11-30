cd7c82d8f9d96c879b8f0b8d8385209d02d58f7f
Rashid Goshtasbi <rgosh001@well.cs.ucr.edu>
2014-11-29 20:45:41 -0800

    did half of assn6

--- assn6/assn6.asm
+++ assn6/assn6.asm
@@ -16,17 +16,13 @@
    ;Instructions
    ;----------------------
       LOOP
-         LD R1, VECTOR     ;inputs vector into R1
-         LD R2, MENU       ;loads MENU output
-         JSRR R2
-         
-         TRAP x20          ;get user input
-         ;TRAP x21          ;outputs user input
+         LD R6, MENU
+         JSRR R6
 
          LD R3, CONVERT    ;convert input to decimal'
          NOT R3, R3
          ADD R3, R3, #1
-         ADD R0, R0, R3    ;puts converted decimal into R0
+         ADD R0, R1, R3    ;puts converted decimal into R0
          
          NOT R0, R0
          ADD R0, R0, #1    ;negates user decimal input to use for BR checks
@@ -57,33 +53,66 @@
             LD R6, ALL_MACHINES_BUSY
             JSRR R6
 
-            LEA R0, NEW_PROMPT
-            TRAP x22
-            BR LOOP
+            ADD R2, R2, #0
+            BRn ALLBUSY
+            BRzp ALLNOTBUSY
+            ALLBUSY
+               LEA R0, ALLBUSYPROMPT
+               TRAP x22
+               LEA R0, NEW_PROMPT
+               TRAP x22
+               BR LOOP
+            ALLNOTBUSY
+               LEA R0, ALLNOTBUSYPROMPT
+               TRAP x22
+               LEA R0, NEW_PROMPT
+               TRAP x22
+               BR LOOP
          END_ALL_MACHINES_BUSY_CALL
 
          ALL_MACHINES_FREE_CALL
             LD R6, ALL_MACHINES_FREE
             JSRR R6
 
-            LEA R0, NEW_PROMPT
-            TRAP x22
-            BR LOOP
+            ADD R2, R2, #0
+            BRn ALLFREE
+            BRzp ALLNOTFREE
+            ALLFREE
+               LEA R0, ALLFREEPROMPT
+               TRAP x22
+               LEA R0, NEW_PROMPT
+               TRAP x22
+               BR LOOP
+            ALLNOTFREE
+               LEA R0, ALLNOTFREEPROMPT
+               TRAP x22
+               LEA R0, NEW_PROMPT
+               TRAP x22
+               BR LOOP
          END_ALL_MACHINES_FREE_CALL
 
          NUM_BUSY_MACHINES_CALL
+            LD R6, NUM_M_B
+            JSRR R6
+
             LEA R0, NEW_PROMPT
             TRAP x22
             BR LOOP
          END_NUM_BUSY_MACHINES_CALL
 
          NUM_FREE_MACHINES_CALL
+            LD R6, NUM_M_F
+            JSRR R6
+
             LEA R0, NEW_PROMPT
             TRAP x22
             BR LOOP
          END_NUM_FREE_MACHINES_CALL
 
          NUM_MACHINE_STATUS_CALL
+            LD R6, M_STATUS
+            JSRR R6
+
             LEA R0, NEW_PROMPT
             TRAP x22
             BR LOOP
@@ -115,26 +144,34 @@
    ;---
    ;LOCAL DATA
    ;---
-      VECTOR            .FILL    #27155
       CONVERT           .FILL    #48
       MENU              .FILL    x3100
       ALL_MACHINES_BUSY .FILL    x3200
       ALL_MACHINES_FREE .FILL    x3300
+      NUM_M_B           .FILL    x3400
+      NUM_M_F           .FILL    x3500
+      M_STATUS          .FILL    x3600
       NEW_PROMPT        .STRINGZ "\n-----------------------------------\n"
       ERROR_PROMPT      .STRINGZ "\nError: Input Not Valid. Please Try Again."
       TEST              .STRINGZ "TEST\n"
       EXIT_PROMPT       .STRINGZ "\nResponse: 7. Goodbye!\n"
+      ALLNOTBUSYPROMPT  .STRINGZ    "ALL NOT BUSY"
+      ALLBUSYPROMPT  .STRINGZ    "ALL BUSY"
+      ALLNOTFREEPROMPT  .STRINGZ    "ALL NOT FREE"
+      ALLFREEPROMPT  .STRINGZ    "ALL FREE"
 
 ;---------------------------------------------------------------------------------   
    ;---
    ; SUBROUTINE #1 -- MENU
+   ; INPUTS: NONE
+   ; POSTCONDITION: PRINT OUT NUMERICAL OPTION MENU
+   ; RETURN VALUE IN R1 IS SELECTION
    ;---
    .ORIG x3100
 
    ;SUBROUTINE INSTRUCTIONS
    ;(1) Backup R7 & any registers the subroutine changes except Return Values
       ST R0, BACKUP_R0_3100
-      ST R1, BACKUP_R1_3100
       ST R2, BACKUP_R2_3100
       ST R3, BACKUP_R3_3100
       ST R4, BACKUP_R4_3100
@@ -143,26 +180,29 @@
       ST R7, BACKUP_R7_3100
 
    ;(2) Subroutine's algorithm
-      LEA R0, MENU_PROMPT
-      TRAP x22
-      LEA R0, MENU1
-      TRAP x22
-      LEA R0, MENU2
-      TRAP x22
-      LEA R0, MENU3
-      TRAP x22
-      LEA R0, MENU4
-      TRAP x22
-      LEA R0, MENU5
-      TRAP x22
-      LEA R0, MENU6
-      TRAP x22
-      LEA R0, MENU7
-      TRAP x22
+         LEA R0, MENU_PROMPT
+         TRAP x22
+         LEA R0, MENU1
+         TRAP x22
+         LEA R0, MENU2
+         TRAP x22
+         LEA R0, MENU3
+         TRAP x22
+         LEA R0, MENU4
+         TRAP x22
+         LEA R0, MENU5
+         TRAP x22
+         LEA R0, MENU6
+         TRAP x22
+         LEA R0, MENU7
+         TRAP x22
+         
+         TRAP x20          ;get user input
+         ADD R1, R0, #0
+
 
    ;(3) Restore Registers
       LD R0, BACKUP_R0_3100
-      LD R1, BACKUP_R1_3100
       LD R2, BACKUP_R2_3100
       LD R3, BACKUP_R3_3100
       LD R4, BACKUP_R4_3100
@@ -175,7 +215,6 @@
 
    ; Subroutine Data
       BACKUP_R0_3100 .BLKW #1
-      BACKUP_R1_3100 .BLKW #1
       BACKUP_R2_3100 .BLKW #1
       BACKUP_R3_3100 .BLKW #1
       BACKUP_R4_3100 .BLKW #1
@@ -195,6 +234,9 @@
 ;---------------------------------------------------------------------------------   
    ;---
    ; SUBROUTINE #2 -- ALL_MACHINES_BUSY
+   ; INPUTS: NONE
+   ; POSTCONDITION: CHECKS TO SEE IF ALL MACHINES ARE BUSY
+   ; RETURN: R2 is 1 if all machines are busy, 0 otherwise.
    ;---
    .ORIG x3200
 
@@ -202,7 +244,6 @@
    ;(1) Backup R7 & any registers the subroutine changes except Return Values
       ST R0, BACKUP_R0_3200
       ST R1, BACKUP_R1_3200
-      ;ST R2, BACKUP_R2_3200
       ST R3, BACKUP_R3_3200
       ST R4, BACKUP_R4_3200
       ST R5, BACKUP_R5_3200
@@ -211,48 +252,31 @@
 
    ;(2) Subroutine's algorithm
       ;0 means busy, 1 means free
-      LD R2, EMPTY1
-
+      LD R1, VECTOR     ;inputs vector into R1
       LD R3, COUNT
+      LD R2, EMPTY1
+      ADD R2, R2, #1
       LOOP1
          ADD R1, R1, #0
-         BRn NEG
-         BRp POS
-         NEG
-            ADD R2, R2, #0    ;1 is busy
-            BR LAST
-         END_NEG
-         POS                  ;0 is free
-            ADD R2, R2, #1
-            BR LAST
-         END_POS
-
-         LAST
-         ADD R1, R1, R1
-
-         ADD R2, R2, #0
-         BRp NOT_ALL_BUSY
-         NOT_ALL_BUSY
-            LEA R0, NOT_BUSY
-            TRAP x22
+         BRn POS
+         BRzp NOT_ALL_BUSY
+         POS 
+            LD R2, EMPTY1
+            ADD R2, R2, #0
             BR END_END
+   
+         NOT_ALL_BUSY
+         ADD R1, R1, R1
 
          ADD R3, R3, #-1
          BRp LOOP1
-         BRnz END_CHECK
-         END_CHECK
-            ADD R2, R2, #0
-            BRp ALL_BUSY_PROMPT
-            ALL_BUSY_PROMPT
-               LEA R0, ALL_BUSY
-               TRAP x22
+         BRnz END_END
       END_END
 
 
    ;(3) Restore Registers
       LD R0, BACKUP_R0_3200
       LD R1, BACKUP_R1_3200
-      ;LD R2, BACKUP_R2_3200
       LD R3, BACKUP_R3_3200
       LD R4, BACKUP_R4_3200
       LD R5, BACKUP_R5_3200
@@ -265,12 +289,12 @@
    ; Subroutine Data
       BACKUP_R0_3200 .BLKW #1
       BACKUP_R1_3200 .BLKW #1
-      ;BACKUP_R2_3200 .BLKW #1
       BACKUP_R3_3200 .BLKW #1
       BACKUP_R4_3200 .BLKW #1
       BACKUP_R5_3200 .BLKW #1
       BACKUP_R6_3200 .BLKW #1
       BACKUP_R7_3200 .BLKW #1
+      VECTOR            .FILL    xABCD
       NOT_BUSY       .STRINGZ    "\nCHOICE 1: Not all machines are busy\n"
       ALL_BUSY       .STRINGZ    "\nCHOICE 1: All machines are busy\n"
       COUNT          .FILL #15
@@ -279,7 +303,10 @@
 
 ;---------------------------------------------------------------------------------   
    ;---
-   ; SUBROUTINE #3 -- ALL_MACHINES_BUSY
+   ; SUBROUTINE #3 -- ALL_MACHINES_FREE
+   ; INPUTS: NONE
+   ; POSTCONDITION: CHECKS TO SEE IF ALL MACHINES ARE FREE
+   ; RETURN: R2 is 1 if all machines are FREE, 0 otherwise.
    ;---
    .ORIG x3300
 
@@ -287,7 +314,6 @@
    ;(1) Backup R7 & any registers the subroutine changes except Return Values
       ST R0, BACKUP_R0_3300
       ST R1, BACKUP_R1_3300
-      ;ST R2, BACKUP_R2_3300
       ST R3, BACKUP_R3_3300
       ST R4, BACKUP_R4_3300
       ST R5, BACKUP_R5_3300
@@ -296,48 +322,30 @@
 
    ;(2) Subroutine's algorithm
       ;0 means busy, 1 means free
-      LD R2, EMPTY11
-
-      LD R3, COUNT1
-      LOOP1
+      LD R1, VECTOR2     ;inputs vector into R1
+      LD R3, COUNT2
+      LD R2, EMPTY2
+      ADD R2, R2, #1
+      LOOP2
          ADD R1, R1, #0
-         BRn NEG1
-         BRp POS1
-         NEG1
-            ADD R2, R2, #1    ;1 is busy
-            BR LAST1
-         END_NEG1
-         POS1                  ;0 is free
+         BRzp POS2
+         BRn NOT_ALL_FREE
+         POS2
+            LD R2, EMPTY2
             ADD R2, R2, #0
-            BR LAST1
-         END_POS1
-
-         LAST1
+            BR END_END2
+   
+         NOT_ALL_FREE
          ADD R1, R1, R1
 
-         ADD R2, R2, #0
-         BRp NOT_ALL_FREE
-         NOT_ALL_FREE
-            LEA R0, NOT_FREE
-            TRAP x22
-            BR END_END
-
          ADD R3, R3, #-1
-         BRp LOOP1
-         BRnz END_CHECK1
-         END_CHECK1
-            ADD R2, R2, #0
-            BRp ALL_FREE_PROMPT
-            ALL_FREE_PROMPT
-               LEA R0, ALL_FREE
-               TRAP x22
-      END_END1
-
+         BRp LOOP2
+         BRnz END_END2
+      END_END2
 
    ;(3) Restore Registers
       LD R0, BACKUP_R0_3300
       LD R1, BACKUP_R1_3300
-      ;LD R2, BACKUP_R2_3300
       LD R3, BACKUP_R3_3300
       LD R4, BACKUP_R4_3300
       LD R5, BACKUP_R5_3300
@@ -350,16 +358,16 @@
    ; Subroutine Data
       BACKUP_R0_3300 .BLKW #1
       BACKUP_R1_3300 .BLKW #1
-      ;BACKUP_R2_3300 .BLKW #1
       BACKUP_R3_3300 .BLKW #1
       BACKUP_R4_3300 .BLKW #1
       BACKUP_R5_3300 .BLKW #1
       BACKUP_R6_3300 .BLKW #1
       BACKUP_R7_3300 .BLKW #1
-      NOT_FREE       .STRINGZ    "\nCHOICE 2: Not all machines are free\n"
-      ALL_FREE       .STRINGZ    "\nCHOICE 2: All machines are free\n"
-      COUNT1          .FILL #15
-      EMPTY11         .FILL #0
+      VECTOR2           .FILL    xABCD
+      NOT_FREE       .STRINGZ    "\nCHOICE 1: Not all machines are busy\n"
+      ALL_FREE       .STRINGZ    "\nCHOICE 1: All machines are busy\n"
+      COUNT2         .FILL #15
+      EMPTY2         .FILL #0
       .END
 
 ;---------------------------------------------------------------------------------   
@@ -372,7 +380,7 @@
    ;(1) Backup R7 & any registers the subroutine changes except Return Values
       ST R0, BACKUP_R0_3400
       ST R1, BACKUP_R1_3400
-      ;ST R2, BACKUP_R2_3400
+      ST R2, BACKUP_R2_3400
       ST R3, BACKUP_R3_3400
       ST R4, BACKUP_R4_3400
       ST R5, BACKUP_R5_3400
@@ -381,48 +389,37 @@
 
    ;(2) Subroutine's algorithm
       ;0 means busy, 1 means free
-      LD R2, EMPTY11
+      LD R2, EMPTY_IT
 
-      LD R3, COUNT1
-      LOOP1
+      LD R3, COUNT_IT
+      LOOP_IT
          ADD R1, R1, #0
-         BRn NEG1
-         BRp POS1
-         NEG1
-            ADD R2, R2, #1    ;1 is busy
-            BR LAST1
-         END_NEG1
-         POS1                  ;0 is free
-            ADD R2, R2, #0
-            BR LAST1
-         END_POS1
-
-         LAST1
-         ADD R1, R1, R1
-
-         ADD R2, R2, #0
-         BRp NOT_ALL_FREE
-         NOT_ALL_FREE
-            LEA R0, NOT_FREE
-            TRAP x22
-            BR END_END
+         BRp POS_IT
+         BRn SKIP_IT
+         POS_IT
+            ADD R2, R2, #1    ;0 is BUSY
+            BR SKIP_IT
+         END_POS_IT
+   
+         SKIP_IT
 
          ADD R3, R3, #-1
-         BRp LOOP1
-         BRnz END_CHECK1
-         END_CHECK1
-            ADD R2, R2, #0
-            BRp ALL_FREE_PROMPT
-            ALL_FREE_PROMPT
-               LEA R0, ALL_FREE
-               TRAP x22
-      END_END1
+         BRp LOOP_IT
+         BRnz END_IT_ALL
+      END_IT_ALL
+
+      LEA R0, NUMBER_BUSY
+      TRAP x22
+
+      LD R3, CONVERT2
+      ADD R0, R2, R3
+      TRAP x21
 
 
    ;(3) Restore Registers
       LD R0, BACKUP_R0_3400
       LD R1, BACKUP_R1_3400
-      ;LD R2, BACKUP_R2_3400
+      LD R2, BACKUP_R2_3400
       LD R3, BACKUP_R3_3400
       LD R4, BACKUP_R4_3400
       LD R5, BACKUP_R5_3400
@@ -435,15 +432,188 @@
    ; Subroutine Data
       BACKUP_R0_3400 .BLKW #1
       BACKUP_R1_3400 .BLKW #1
-      ;BACKUP_R2_3400 .BLKW #1
+      BACKUP_R2_3400 .BLKW #1
       BACKUP_R3_3400 .BLKW #1
       BACKUP_R4_3400 .BLKW #1
       BACKUP_R5_3400 .BLKW #1
       BACKUP_R6_3400 .BLKW #1
       BACKUP_R7_3400 .BLKW #1
-      NUM_BUSY       .STRINGZ    "\nCHOICE 2: Number of machines busy is \n"
-      COUNT1          .FILL #15
-      EMPTY11         .FILL #0
+      CONVERT2 .FILL x30
+      NUMBER_BUSY       .STRINGZ    "Number of machines busy: "
+      EMPTY_IT         .FILL #0
+      COUNT_IT       .FILL    #15
+      .END
+
+;---------------------------------------------------------------------------------   
+   ;---
+   ; SUBROUTINE #5 -- NUM_FREE_MACHINES
+   ;---
+   .ORIG x3500
+
+   ;SUBROUTINE INSTRUCTIONS
+   ;(1) Backup R7 & any registers the subroutine changes except Return Values
+      ST R0, BACKUP_R0_3500
+      ST R1, BACKUP_R1_3500
+      ST R2, BACKUP_R2_3500
+      ST R3, BACKUP_R3_3500
+      ST R4, BACKUP_R4_3500
+      ST R5, BACKUP_R5_3500
+      ST R6, BACKUP_R6_3500
+      ST R7, BACKUP_R7_3500
+
+   ;(2) Subroutine's algorithm
+      ;0 means busy, 1 means free
+      LD R1, EMPTY_IT1
+
+      LD R3, COUNT_IT1
+      LOOP_IT1
+         ADD R2, R2, #0
+         BRn NEG_IT
+         BRp SKIP_IT1
+         NEG_IT
+            ADD R1, R1, #1    ;0 is BUSY
+            BR SKIP_IT1
+         END_NEG_IT
+   
+         SKIP_IT1
+
+         ADD R3, R3, #-1
+         BRp LOOP_IT1
+         BRnz END_IT_ALL1
+      END_IT_ALL1
+
+      LEA R0, NUMBER_FREE
+      TRAP x22
+
+      LD R3, CONVERT1
+      ADD R0, R1, R3
+      TRAP x21
+
+
+   ;(3) Restore Registers
+      LD R0, BACKUP_R0_3500
+      LD R1, BACKUP_R1_3500
+      LD R2, BACKUP_R2_3500
+      LD R3, BACKUP_R3_3500
+      LD R4, BACKUP_R4_3500
+      LD R5, BACKUP_R5_3500
+      LD R6, BACKUP_R6_3500
+      LD R7, BACKUP_R7_3500
+
+   ;(4) Return
+      RET
+
+   ; Subroutine Data
+      BACKUP_R0_3500 .BLKW #1
+      BACKUP_R1_3500 .BLKW #1
+      BACKUP_R2_3500 .BLKW #1
+      BACKUP_R3_3500 .BLKW #1
+      BACKUP_R4_3500 .BLKW #1
+      BACKUP_R5_3500 .BLKW #1
+      BACKUP_R6_3500 .BLKW #1
+      BACKUP_R7_3500 .BLKW #1
+      NUMBER_FREE       .STRINGZ    "Number of machines free: "
+      EMPTY_IT1         .FILL #0
+      COUNT_IT1       .FILL    #15
+      CONVERT1    .FILL x30
+      .END
+;---------------------------------------------------------------------------------   
+   ;---
+   ; SUBROUTINE #6 -- MACHINE_STATUS
+   ;---
+   .ORIG x3600
+
+   ;SUBROUTINE INSTRUCTIONS
+   ;(1) Backup R7 & any registers the subroutine changes except Return Values
+      ST R0, BACKUP_R0_3600
+      ST R1, BACKUP_R1_3600
+      ST R2, BACKUP_R2_3600
+      ST R3, BACKUP_R3_3600
+      ST R4, BACKUP_R4_3600
+      ST R5, BACKUP_R5_3600
+      ST R6, BACKUP_R6_3600
+      ST R7, BACKUP_R7_3600
+
+   ;(2) Subroutine's algorithm
+      ;0 means busy, 1 means free
+      LD R1, EMPTY12
+
+      LD R3, COUNT12
+      LEA R0, N_MACHINE
+      TRAP x22
+
+      TRAP x20
+      NOT R0, R0
+      ADD R0, R0, #1
+
+      ADD R3, R3, R0
+      LD R4, EMPTY12
+
+      LOOP12
+         ADD R2, R2, R2
+
+         ADD R3, R3, #-1
+         BRn STATUS_CHECK
+         BRzp LOOP12
+         STATUS_CHECK
+            ADD R2, R2, #0
+            BRn NEG_STATUS
+            BRp POS_STATUS
+            NEG_STATUS
+               ADD R4, R4, #1    ;1 is busy
+               BR END_END12   
+            POS_STATUS
+               ADD R4, R4, #1    ;0 is FREE
+               BR END_END12
+            END_POS_STATUS
+         END_STATUS_CHECK
+      END_END12
+
+      LEA R0, N_MACHINE_STATUS
+      TRAP x22
+
+      ADD R4, R4, #0
+      BRn BUSY_OUT
+      BRp FREE_OUT
+      BUSY_OUT
+         LEA R0, STAT_BUSY
+         TRAP x22
+         BR FINISHED
+      FREE_OUT
+         LEA R0, STAT_FREE
+         TRAP x22
+         BR FINISHED
+
+      FINISHED
+
+   ;(3) Restore Registers
+      LD R0, BACKUP_R0_3600
+      LD R1, BACKUP_R1_3600
+      LD R2, BACKUP_R2_3600
+      LD R3, BACKUP_R3_3600
+      LD R4, BACKUP_R4_3600
+      LD R5, BACKUP_R5_3600
+      LD R6, BACKUP_R6_3600
+      LD R7, BACKUP_R7_3600
+
+   ;(4) Return
+      RET
+
+   ; Subroutine Data
+      BACKUP_R0_3600 .BLKW #1
+      BACKUP_R1_3600 .BLKW #1
+      BACKUP_R2_3600 .BLKW #1
+      BACKUP_R3_3600 .BLKW #1
+      BACKUP_R4_3600 .BLKW #1
+      BACKUP_R5_3600 .BLKW #1
+      BACKUP_R6_3600 .BLKW #1
+      BACKUP_R7_3600 .BLKW #1
+      N_MACHINE         .STRINGZ    "Enter Machine Number To Check Status: "
+      N_MACHINE_STATUS  .STRINGZ    "Status: "
+      STAT_BUSY         .STRINGZ    "BUSY\n"
+      STAT_FREE         .STRINGZ    "FREE\n"
+      COUNT12          .FILL #15
+      EMPTY12         .FILL #0
       .END
 
 ;---------------------------------------------------------------------------------   
